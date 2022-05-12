36e8978df25f374334de813943e396451e984fc4
zsimjee <zaydsimjee@gmail.com>
2022-05-12 00:46:17 -0700

    Mysql env vars

--- src/config/mysql.ts
+++ src/config/mysql.ts
@@ -1,11 +1,11 @@
 import * as mysql from 'mysql2';
 
 const basePool = mysql.createPool({
-  host: process.env.PG_HOST || '0.0.0.0',
-  port: parseInt(process.env.PG_PORT || '5432'),
-  user: process.env.PG_USERNAME || 'postgres',
-  password: process.env.PG_PASSWORD || 'postgres',
-  database: process.env.PG_DATABASE || 'postgres',
+  host: process.env.MYSQL_HOST || '0.0.0.0',
+  port: parseInt(process.env.MYSQL_PORT || '3306'),
+  user: process.env.MYSQL_USERNAME || 'mysql',
+  password: process.env.MYSQL_PASSWORD || 'mysql',
+  database: process.env.MYSQL_DATABASE || 'mysql',
 });
 
 export const pool = basePool.promise();
